eeae901059e14b76d28d14f82efddcb18f23d3c4
conradoantonio <anton_con@hotmail.com>
2018-02-28 16:31:26 -0600

    Primer commit

---
+++ prototype.sql
@@ -0,0 +1,268 @@
+/*
+SQLyog Ultimate v9.63 
+MySQL - 5.5.5-10.1.29-MariaDB : Database - prototype
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`prototype` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `prototype`;
+
+/*Table structure for table `addresses` */
+
+DROP TABLE IF EXISTS `addresses`;
+
+CREATE TABLE `addresses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `receiver` varchar(255) DEFAULT NULL,
+  `street` varchar(255) NOT NULL,
+  `colony` varchar(200) DEFAULT NULL,
+  `out_num` varchar(10) DEFAULT NULL,
+  `int_num` varchar(10) DEFAULT NULL,
+  `state` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `country` varchar(10) NOT NULL,
+  `postal_code` varchar(10) NOT NULL,
+  `residential` tinyint(4) DEFAULT NULL,
+  `main` tinyint(4) DEFAULT NULL,
+  `latitude` varchar(100) NOT NULL,
+  `longitude` varchar(100) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `addresses` */
+
+/*Table structure for table `banners` */
+
+DROP TABLE IF EXISTS `banners`;
+
+CREATE TABLE `banners` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `img` varchar(50) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `banners` */
+
+/*Table structure for table `cards` */
+
+DROP TABLE IF EXISTS `cards`;
+
+CREATE TABLE `cards` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `type` varchar(30) NOT NULL,
+  `last_digits_card` varchar(4) NOT NULL,
+  `token` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `cards` */
+
+/*Table structure for table `faqs` */
+
+DROP TABLE IF EXISTS `faqs`;
+
+CREATE TABLE `faqs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question` text NOT NULL,
+  `answer` text NOT NULL,
+  `img` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `faqs` */
+
+/*Table structure for table `genre` */
+
+DROP TABLE IF EXISTS `genre`;
+
+CREATE TABLE `genre` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `genre` */
+
+/*Table structure for table `log_ins` */
+
+DROP TABLE IF EXISTS `log_ins`;
+
+CREATE TABLE `log_ins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `date_log` date DEFAULT NULL,
+  `date_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `log_ins` */
+
+/*Table structure for table `news` */
+
+DROP TABLE IF EXISTS `news`;
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `content` text,
+  `img` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `news` */
+
+/*Table structure for table `notice_privacy` */
+
+DROP TABLE IF EXISTS `notice_privacy`;
+
+CREATE TABLE `notice_privacy` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `notice_privacy` */
+
+/*Table structure for table `roles` */
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `roles` */
+
+insert  into `roles`(`id`,`role`) values (1,'Administrador');
+
+/*Table structure for table `service_details` */
+
+DROP TABLE IF EXISTS `service_details`;
+
+CREATE TABLE `service_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `service_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `img` varchar(100) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `service_details` */
+
+/*Table structure for table `services` */
+
+DROP TABLE IF EXISTS `services`;
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `customer_name` varchar(100) DEFAULT NULL,
+  `customer_email` varchar(100) DEFAULT NULL,
+  `conekta_order_id` varchar(255) DEFAULT NULL,
+  `conekta_customer_id` varchar(255) DEFAULT NULL,
+  `total` double DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `receiver` varchar(255) DEFAULT NULL,
+  `street` text,
+  `colony` varchar(100) DEFAULT NULL,
+  `out_num` varchar(10) DEFAULT NULL,
+  `int_num` varchar(10) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `country` varchar(5) DEFAULT NULL,
+  `postal_code` varchar(10) DEFAULT NULL,
+  `datetime_formated` varchar(200) DEFAULT NULL,
+  `start_datetime` timestamp NULL DEFAULT NULL,
+  `end_datetime` timestamp NULL DEFAULT NULL,
+  `card_id` int(11) DEFAULT NULL,
+  `deliever_id` int(11) DEFAULT '0',
+  `status_id` varchar(30) DEFAULT NULL,
+  `payment_method` varchar(20) DEFAULT NULL,
+  `is_finished` tinyint(4) DEFAULT '0',
+  `comment` text,
+  `last_digits_card` varchar(10) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `services` */
+
+/*Table structure for table `statuses` */
+
+DROP TABLE IF EXISTS `statuses`;
+
+CREATE TABLE `statuses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `statuses` */
+
+insert  into `statuses`(`id`,`name`) values (1,'Activo'),(2,'Pendiente'),(3,'Rechazado'),(4,'Bloqueado');
+
+/*Table structure for table `terms_conditions` */
+
+DROP TABLE IF EXISTS `terms_conditions`;
+
+CREATE TABLE `terms_conditions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `terms_conditions` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `img` varchar(100) NOT NULL,
+  `phone` varchar(18) DEFAULT NULL,
+  `conekta_customer_id` varchar(255) DEFAULT NULL,
+  `role_id` tinyint(4) DEFAULT NULL,
+  `social_network` tinyint(4) DEFAULT NULL,
+  `player_id` varchar(155) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT '1',
+  `remember_token` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ resources/views/login.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Login</title>
+
+        <link rel="stylesheet" href="{{ asset('plugins/pace/pace-theme-flash.css')}}"  type="text/css" media="screen"/>
+        <link rel="stylesheet" href="{{ asset('plugins/boostrapv3/css/bootstrap.min.css')}}"  type="text/css"/>
+        <link rel="stylesheet" href="{{ asset('css/style.css')}}" type="text/css"/>
+        <style type="text/css">
+        /* Change the white to any color ;) */
+        input:-webkit-autofill {
+            -webkit-box-shadow: 0 0 0px 1000px white inset !important;
+        }
+        </style>
+    </head>
+    <body>
+        <div class="container centered">
+            <div class="col-lg-12 text-center">
+                <div class="row tiles-container m-b-10">
+                    <div class="col-xs-12 col-sm-8 col-sm-push-2 col-sm-pull-2 col col-md-6 col-md-push-3 col-md-pull-3">
+                        <div class="tiles white p-t-20 p-l-15 p-r-15 p-b-30">
+                            <h2 class="text-center">Login <span class="semi-bold text-success">Prototype</span></h2>
+                            <form class="m-t-30 m-l-15 m-r-15" method="POST" action="login" autocomplete="off">
+                                {!! csrf_field() !!}
+                                <div class="form-group {{ $errors->has('correo') || $errors->has('status') ? ' error' : '' }}" style="padding-bottom: 1px;">
+                                    <div class="controls">
+                                        <input type="text" class="form-control" id="correo" name="correo" value="{{ @session('account') ? session('account') : '' }}" placeholder="Correo">
+                                        @if ($errors->has('correo'))
+                                            <span class="show-error">
+                                                <strong>{{ $errors->first('correo') }}</strong>
+                                            </span>
+                                        @endif
+                                        @if ($errors->has('status'))
+                                            <span class="show-error">
+                                                <strong>{{ $errors->first('status') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-label">Contraseña</label>
+                                    <div class="controls">
+                                        <input type="password" class="form-control" id="password" name="password" placeholder="Escribe tu contraseña">
+                                    </div>
+                                </div>
+                                <button class="btn btn-block btn-primary m-t-10" type="submit"><i class="icon-cloud-download"></i>Entrar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        
+        <script src="{{ asset('js/jquery.js') }}"></script>
+        <script src="{{ asset('js/sweetalert.min.js') }}"></script>
+        <script src="{{ asset('plugins/boostrapv3/js/bootstrap.min.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('plugins/pace/pace.min.js') }}" type="text/javascript"></script>
+    </body>
+</html>
